0714e2a5851531f41235a0b0a9bcfba2ebaa5cc3
Uhmopt <qethum@gmail.com>
2025-03-09 21:44:45 +0800

    [Fix]TypeORM config remove for use prisma

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { TasksModule } from './tasks/tasks.module';
@@ -9,12 +8,6 @@ dotenv.config();
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      url: process.env.DATABASE_URL,
-      autoLoadEntities: true,
-      synchronize: true, // Set to false in production
-    }),
     PrismaModule,
     AuthModule,
     TasksModule,
